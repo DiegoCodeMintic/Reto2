ef023e72e11eec53309c29691fe5b2b4c8c57dda
Didark8810 <diegobernal8810@hotmail.com>
2021-10-19 15:00:20 -0500

    prepublicacion front end

--- js/utilidades.js
+++ js/utilidades.js
@@ -1,14 +1,16 @@
-var urlClientes='https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client';
+var urlClientes = 'https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client';
 
-var urldoctors ='https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/doctor/doctor';
+var urldoctors = 'https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/doctor/doctor';
 
-var urlMensajes ='https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/message/message';
+var urlMensajes = 'https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/message/message';
 
 //nuevo 
+var urlBase = 'http://132.226.254.69:8080';
+//var urlBase = 'http://localhost:8080';
 
-var urlEspecialidad = 'http://localhost:8080/api/Specialty';
-var urlDoctor = 'http://localhost:8080/api/Doctor';
-var urlCliente = 'http://localhost:8080/api/Client';
-var urlMessage ='http://localhost:8080/api/Message';
-var urlReservation = 'http://localhost:8080/api/Reservation';
-var ulrAdminUser = 'http://localhost:8080/api/AdminUser';
+var urlEspecialidad = urlBase + '/api/Specialty';
+var urlDoctor       = urlBase + '/api/Doctor';
+var urlCliente      = urlBase + '/api/Client';
+var urlMessage      = urlBase + '/api/Message';
+var urlReservation  = urlBase + '/api/Reservation';
+var ulrAdminUser    = urlBase + '/api/AdminUser';
