bdfe8ffad6a664675948ea121d89b8d41d6a555d
Diego Bernal <diegobernal8810@hotmail.com>
2021-10-28 21:16:02 -0500

    ultimo avance antes de hacer branch a reto4

--- js/utilidades.js
+++ js/utilidades.js
@@ -1,10 +1,4 @@
-var urlClientes = 'https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client';
 
-var urldoctors = 'https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/doctor/doctor';
-
-var urlMensajes = 'https://g8b329f59e9c137-db202109230709.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/message/message';
-
-//nuevo 
 var urlBase = 'http://150.230.69.0:8080';
 //var urlBase = 'http://localhost:8080';
 
