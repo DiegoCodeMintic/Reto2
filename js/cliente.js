e6332768a4abf52835efeed600ea14aa29c62fe6
Diego Bernal <diegobernal8810@hotmail.com>
2021-10-29 11:35:34 -0500

    pre publicacion

--- js/cliente.js
+++ js/cliente.js
@@ -116,6 +116,8 @@ function guardarDatos() {
         });
     } else {
         //editar
+
+        
         url = urlCliente + "/update";
         let id = currentCliente;
         var data = { idClient: id, name: nombre, email: email, password: pass, age: edad };
